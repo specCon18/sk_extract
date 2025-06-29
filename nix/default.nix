a589331d4bbd36c694dd2a5c1e488734276e4a1d
Jermeiah S <owner@arouzing.xyz>
2025-06-28 21:03:02 -0400

    improved derrivation to be idomatic

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,23 @@
-{ pkgs ? import <nixpkgs> { }, lib }:
+{
+  unrar,
+  xz,
+  pkg-config,
+  rustPlatform,
+  lib,
+}:
 
-pkgs.rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   pname = "sk_extract";
   version = "0.1.0";
   cargoLock.lockFile = ../Cargo.lock;
-  src = pkgs.lib.cleanSource ../.;
-  buildInputs = [ pkgs.xz pkgs.unrar ];
-  nativeBuildInputs = [ pkgs.xz pkgs.pkg-config ];
-#  doCheck = false;
+  src = lib.cleanSource ../.;
+  buildInputs = [
+    xz
+    unrar
+  ];
+  nativeBuildInputs = [
+    xz
+    pkg-config
+  ];
+  doCheck = false;
 }
